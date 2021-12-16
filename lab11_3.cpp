f93924386d345f7a69e7306f919ce9f84c8841c7
BestChennn <bestchenbestchen@gmail.com>
2021-12-16 16:56:05 +0700

    Complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,37 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float mean ,sum,sum1,SD ; 
+    
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline))
+        {
+            sum += atof(textline.c_str());
+            sum1 += pow(atof(textline.c_str()),2);
+            count++;
+        }
+
+        
+        mean = sum/count;
+        SD = sqrt((sum1/count)-pow(mean,2));
+
+        cout << "Number of data = "<< count <<'\n';
+        cout << setprecision(3) << "Mean = "<< mean <<'\n';
+        cout << setprecision(3) << "Standard deviation = "<< SD ;
+
+    source.close();
+    return 0;
+    }
+
+
+
+
+
